3acd29907e65324bf745ebb129e69e2eed308e37
Huy Nguyen Nhat <nhathuy709@gmail.com>
2025-12-09 20:50:50 +0700

    order-module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './modules/product/product.module';
+import { OrderModule } from './modules/order/order.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
